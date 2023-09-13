de18c757f4a74743e7323d1815ed16f903a5a8cd
maiquelfx <144914257+maiquelfx@users.noreply.github.com>
2023-09-13 16:13:53 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -1,19 +1,11 @@
 #pragma once // prevents multiple definitions
-
 #include <iostream> // print library
 
 // Exercise 1: implements function 'add', that adds two numbers
 int add(int a, int b)
-{
-   return a+b; 
-}
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
-{
-return a*b; 
-}
-
-// ===============
+ // ===============
 // implementations
 
 int add(int a, int b)
