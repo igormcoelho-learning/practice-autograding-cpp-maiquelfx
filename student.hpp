64b9c0f2cad0a923b6b678e345708c1a622ff4e8
maiquelfx <144914257+maiquelfx@users.noreply.github.com>
2023-09-13 16:23:01 -0300

    vírgulas

--- student.hpp
+++ student.hpp
@@ -1,20 +1,23 @@
 #pragma once // prevents multiple definitions
+
 #include <iostream> // print library
 
 // Exercise 1: implements function 'add', that adds two numbers
-int add(int a, int b)
+int add(int a, int b);
+
 // Exercise 2: implements function 'mul', that multiplies two numbers
-int mul(int a, int b)
+int mul(int a, int b);
+
  // ===============
 // implementations
 
 int add(int a, int b)
 {
-   return a+b;  // TODO: fix
+   return a+b;  
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
-   return a*b; // TODO: fix
+   return a*b; 
 }
